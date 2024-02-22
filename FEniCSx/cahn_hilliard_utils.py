af548512f0e5b1e44d90ae8f2e6ca922bf717182
Tom Doerffel <tom.doerffel@fu-berlin.de>
2024-02-22 17:08:50 +0100

    add all relevant files

---
+++ FEniCSx/cahn_hilliard_utils.py
@@ -0,0 +1,66 @@
+import dolfinx as dfx
+
+import numpy as np
+
+import ufl
+
+
+def cahn_hilliard_form(psi, psi0, dt,
+                       metric_factor=0,
+                       M=lambda c: 1,
+                       c_of_y=lambda y: y,
+                       free_energy=lambda c: 0.25 * (c**2 - 1)**2,
+                       lam=0.01,
+                       I_charge=0.1,
+                       theta=0.5):
+
+    # TODO:
+    #   [ ] implement radial volume and surface element
+    #   [ ] Assert whether psi, psi0, and v are on the same mesh/V
+    V = psi.function_space
+    mesh = V.mesh
+
+    # Split the functions
+    y, mu = ufl.split(psi)
+    y0, mu0 = ufl.split(psi0)
+
+    y = ufl.variable(y)
+    c = c_of_y(y)
+
+    dcdy = ufl.diff(c, y)
+
+    v_c, v_mu = ufl.TestFunctions(V)
+
+    # Differentiate the free energy function to
+    # obtain the chemical potential
+    c = ufl.variable(c)
+    dfdc = ufl.diff(free_energy(c), c)
+    mu_chem = dfdc
+
+    # Theta scheme
+    theta = dfx.fem.Constant(mesh, theta)
+
+    mu_theta = theta * mu + (theta - 1.) * mu0
+
+    r = ufl.SpatialCoordinate(mesh)
+
+    # adaptation of the volume element due to geometry
+    s_V = 4 * np.pi * r**2
+    s_A = 2 * np.pi * r**2
+
+    dx = ufl.dx  # The volume element
+    ds = ufl.ds  # The surface element
+
+    flux = M(c) * ufl.grad(mu_theta)
+
+    F1 = s_V * dcdy * (y - y0) * v_c * dx
+    F1 += s_V * ufl.dot(flux, ufl.grad(v_c)) * dt * dx
+    F1 -= s_A * I_charge * v_c * dt * ds
+
+    F2 = s_V * mu * v_mu * dx
+    F2 -= s_V * mu_chem * v_mu * dx
+    F2 -= s_V * ufl.inner(lam * ufl.grad(c), ufl.grad(v_mu)) * dx
+
+    F = F1 + F2
+
+    return F
---
+++ FEniCSx/fenicsx_utils.py
@@ -0,0 +1,245 @@
+import abc
+
+import dolfinx as dfx
+from dolfinx.nls.petsc import NewtonSolver as NewtonSolverBase
+
+import numpy as np
+
+from petsc4py import PETSc
+
+
+def evaluation_points_and_cells(mesh, x):
+    """points_on_proc and cells to be used with dfx.fem.Function.eval()."""
+
+    points = np.zeros((3, len(x)))
+    points[0] = x
+
+    bb_tree = dfx.geometry.bb_tree(mesh, mesh.topology.dim)
+
+    cells = []
+    points_on_proc = []
+    # Find cells whose bounding-box collide with the the points
+    cell_candidates = dfx.geometry.compute_collisions_points(bb_tree, points.T)
+    # Choose one of the cells that contains the point
+    colliding_cells = dfx.geometry.compute_colliding_cells(
+        mesh, cell_candidates, points.T
+    )
+    for i, point in enumerate(points.T):
+        if len(colliding_cells.links(i)) > 0:
+            points_on_proc.append(point)
+            cells.append(colliding_cells.links(i)[0])
+    return points_on_proc, cells
+
+
+def get_mesh_spacing(mesh):
+
+    tdim = mesh.topology.dim
+    num_cells = mesh.topology.index_map(tdim).size_local
+    h = mesh.h(tdim, range(num_cells))
+
+    dx_cell = h.min()
+
+    return dx_cell
+
+
+def time_stepping(
+    solver, u, u0, T, n_out, dt, t_start=0, dt_max=10.,
+    event_handler=lambda t, u, **pars: None,
+    output=None,
+    runtime_analysis=None,
+    **pars,
+):
+
+    it_out = 0
+
+    # Empty containers for output data
+    u_out = []
+    t_out = []
+
+    t = t_start
+
+    V = u.function_space
+    mesh = V.mesh
+
+    # Necessary for output.
+    # TODO: Retrieve interval from mesh
+    x_out = np.linspace(0, 1, 101)
+    points_on_proc, cells = evaluation_points_and_cells(mesh, x_out)
+
+    # Write the initial data.
+    u_out.append(u.sub(0).eval(points_on_proc, cells))
+    t_out.append(t)
+    it_out += 1
+
+    while t < T:
+        u0.x.array[:] = u.x.array[:]
+
+        event_handler(t, u, **pars)
+
+        try:
+            if float(dt) < 1e-6:
+                u_out.append(u.sub(0).eval(points_on_proc, cells))
+                t_out.append(t)
+
+                raise ValueError(f"Timestep too small (dt={dt.value})!")
+
+            iterations, success = solver.solve(u)
+
+        except RuntimeError as e:
+
+            print(e)
+
+            # reset and continue with smaller time step.
+            u.x.array[:] = u0.x.array[:]
+
+            if dt.value < dt_max:
+                dt.value *= 0.5
+
+            print(f"Decrease timestep to dt={dt.value:1.3e}")
+
+            continue
+
+        except ValueError as e:
+
+            print(e)
+
+            break
+
+        if output is not None:
+            output.save_snapshot(u, t)
+
+        if runtime_analysis is not None:
+            runtime_analysis.analyze(u, t)
+
+        t += float(dt)
+
+        if t > it_out / n_out * T:
+            print(f">>> output #{it_out:>4}")
+
+            u_out.append(u.sub(0).eval(points_on_proc, cells))
+            t_out.append(t)
+            it_out += 1
+
+        dt.value *= 1.01
+
+        print(f"t = {t:1.6f} : dt = {dt.value:1.3e}, its = {iterations}")
+
+    return t_out, x_out, u_out
+
+
+class NewtonSolver(NewtonSolverBase):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.convergence_criterion = "incremental"
+        self.rtol = 1e-6
+
+        # # We can customize the linear solver used inside the NewtonSolver by
+        # # modifying the PETSc options
+        ksp = self.krylov_solver
+        opts = PETSc.Options()  # type: ignore
+        option_prefix = ksp.getOptionsPrefix()
+        opts[f"{option_prefix}ksp_type"] = "preonly"
+        opts[f"{option_prefix}pc_type"] = "lu"
+        ksp.setFromOptions()
+
+
+class OutputBase(abc.ABC):
+
+    def __init__(self, u_state, ts_out, *args, **kwargs):
+
+        # Store the state and the output times.
+        self.u_state = u_state
+        self.ts_out_planned = ts_out
+
+        # output counter
+        self.it_out = 0
+        self.t_out_last = None
+        self.t_out_next = ts_out[0]
+
+        self.output_container = []
+        self.output_times = []
+
+        self.setup(*args, **kwargs)
+
+    @abc.abstractmethod
+    def setup(self, *args, **kwargs):
+        pass
+
+    def save_snapshot(self, u_state, t):
+
+        if t > self.t_out_next:
+
+            print(f">>> Save snapshot [{self.it_out:04}] t={t:1.3f}")
+
+            self.output_container.append(self.extract_output(u_state, t))
+            self.output_times.append(t)
+
+            self.it_out += 1
+            self.t_out_last = self.t_out_next
+            self.t_out_next = self.ts_out_planned[self.it_out]
+
+    @abc.abstractmethod
+    def extract_output(self, u_state, t):
+        pass
+
+    def get_output(self, return_time=False):
+
+        if return_time:
+            return self.output_times, self.output_container
+        else:
+            return self.output_container
+
+
+class Fenicx1DOutput(OutputBase):
+
+    def setup(self, x):
+
+        V = self.u_state.function_space
+
+        mesh = V.mesh
+
+        points_on_proc, cells = evaluation_points_and_cells(mesh, x)
+
+        self.x_eval, self.cells = points_on_proc, cells
+
+    def extract_output(self, u_state, t):
+
+        # TODO: Make sure self.u_state and u_state are consistent.
+        V = self.u_state.function_space
+
+        num_comp = V.num_sub_spaces
+
+        output_snapshot = []
+
+        for i_comp in range(num_comp):
+
+            sub_space, dofs = V.sub(i_comp).collapse()
+
+            values = u_state.sub(i_comp).eval(self.x_eval, self.cells)
+
+            output_snapshot.append(values)
+
+        return output_snapshot
+
+
+class RuntimeAnalysisBase(abc.ABC):
+
+    def __init__(self, *args, **kwargs):
+
+        # Initialize empty containers
+        self.t = []
+        self.data = []
+
+        self.setup(*args, **kwargs)
+
+    @abc.abstractmethod
+    def setup(self, *args, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def analyze(self, u_state, t):
+
+        self.t.append(t)
