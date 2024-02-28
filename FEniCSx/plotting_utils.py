8da2821984dfc5d9b790f1913ba3b514a94e7fe0
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-02-28 18:26:00 +0100

    add file pltting_utils.py and add_arrow function

---
+++ FEniCSx/plotting_utils.py
@@ -0,0 +1,40 @@
+# file plotting_utils.py
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def add_arrow(line, position=None, direction='right', size=15, color=None):
+    """
+    add an arrow to a line.
+    
+    Copied from https://stackoverflow.com/questions/34017866/arrow-on-a-line-plot (2024/02/28)
+
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    if color is None:
+        color = line.get_color()
+
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if position is None:
+        position = xdata.mean()
+    # find closest index
+    start_ind = np.argmin(np.absolute(xdata - position))
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops=dict(arrowstyle="->", color=color),
+        size=size
+    )
+    
