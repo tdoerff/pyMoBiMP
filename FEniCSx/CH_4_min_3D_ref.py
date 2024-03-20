f8f917d99175cbf1ef0f33d490075adbff717c90
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-03-20 20:29:38 +0100

    add 3d simulation test

---
+++ FEniCSx/CH_4_min_3D_ref.py
@@ -0,0 +1,173 @@
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import dolfinx as dfx
+
+from dolfinx.fem.petsc import NonlinearProblem
+
+# %matplotlib widget
+from matplotlib import pyplot as plt
+# plt.style.use('fivethirtyeight')
+
+from mpi4py import MPI
+
+import numpy as np
+
+import pyvista
+
+import ufl
+
+from cahn_hilliard_utils import (
+    charge_discharge_stop,
+    AnalyzeOCP,
+    c_of_y,
+    populate_initial_data)
+
+from fenicsx_utils import (get_mesh_spacing,
+                           time_stepping,
+                           NewtonSolver,
+                           FileOutput)
+
+from gmsh_utils import dfx_spherical_mesh
+
+comm_world = MPI.COMM_WORLD
+
+def log(*args, **kwargs):
+    if comm_world.rank == 0:
+        print(*args, **kwargs)
+
+# %%
+# create the mesh
+
+mesh, ct, ft = dfx_spherical_mesh(comm_world, resolution=0.5)
+
+# %%
+# Discretization details
+# ----------------------
+
+# spatial
+dx_cell = get_mesh_spacing(mesh)
+
+log(f"Cell spacing: h = {dx_cell}")
+
+# temporal
+dt = dfx.fem.Constant(mesh, dx_cell * 0.01)
+
+log(f"timestep size: dt = {dt.value:1.3e}")
+
+# %%
+elem1 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+
+mixed_element = elem1 * elem1
+
+V = dfx.fem.FunctionSpace(mesh, mixed_element)  # A mixed two-component function space
+
+# %%
+# The mixed-element functions
+u = dfx.fem.Function(V)
+u0 = dfx.fem.Function(V)
+
+# %%
+# Compute the chemical potential df/dc
+a = 6. / 4
+b = 0.2
+cc = 5
+
+# a = 5. # 6. / 4
+# b = 0. # 0.2
+# cc = 0 # 5
+
+eps = 1e-3
+
+free_energy = lambda u, log, sin: u * log(u) + (1-u) * log(1-u) + a * u * (1 - u) + b * sin(cc * np.pi * u)
+
+# %%
+# Experimental setup
+# ------------------
+
+T_final = 0.2 # ending time
+
+# charging current
+I_charge = dfx.fem.Constant(mesh, 0.01)
+
+def experiment(t, u, I_charge, **kwargs):
+
+    return charge_discharge_stop(t, u, I_charge, c_of_y=c_of_y, **kwargs)
+
+event_params = dict(I_charge=I_charge, stop_at_empty=False, cycling=False)
+
+# %%
+# The variational form
+# --------------------
+from cahn_hilliard_utils import cahn_hilliard_form
+# c_of_y=lambda y: y
+
+params = dict(I_charge=I_charge)
+
+form_weights = dict(surface=1., volume=1.)
+
+F = cahn_hilliard_form(
+    u,
+    u0,
+    dt,
+    free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
+    theta=0.75,
+    c_of_y=c_of_y,
+    M=lambda c: 1. * c * (1 - c),
+    lam=0.1,
+    form_weights=form_weights,
+    **params
+)
+
+# %%
+# Initial data
+# ------------
+
+log(">>> Setting up initial data ...")
+
+u_ini = dfx.fem.Function(V)
+
+# Constant
+c_ini_fun = lambda x: eps * np.ones_like(x[0])
+
+# Initial charge distribution.
+# c_ini_fun = lambda x: eps + 0.5 * np.sin(np.pi * x[0])
+
+populate_initial_data(
+    u_ini, c_ini_fun, lambda c: free_energy(c, ufl.ln, ufl.sin))
+
+# %%
+
+log(">>> Setting up the solver ...")
+
+problem = NonlinearProblem(F, u)
+
+solver = NewtonSolver(comm_world, problem)
+
+# %%
+
+log(">>> Running the simulation ...")
+
+u.interpolate(u_ini)
+
+n_out = 51
+
+output = FileOutput(u, np.linspace(0, T_final, 51), filename="simulation_output/CH_4_true_3d.bp")
+
+rt_analysis = AnalyzeOCP(c_of_y=c_of_y)
+
+time_stepping(
+    solver,
+    u,
+    u0,
+    T_final,
+    dt,
+    dt_increase=1.01,
+    dt_max=1e-2,
+    event_handler=experiment,
+    output=output,
+    runtime_analysis=rt_analysis,
+    **event_params,
+)
