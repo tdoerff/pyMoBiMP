e702d4284749e083fbe846f0481cc1a0fa13200d
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-03-26 11:51:39 +0100

    rename dir and create pyMoBiMP package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="pyMoBiMP",
+    version="0.1",
+    description="Tools to for a single-particle Cahn-Hilliard battery model",
+    url="#",
+    author="Tom Doerffel",
+    author_email="tom.doerffel@wias-berlin.de",
+    license="MIT",
+    packages=["pyMoBiMP"],
+    zip_safe=False,
+)
