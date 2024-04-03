93e13bc34eee7b67b903584472fa2cb53e786da6
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-04-03 18:56:09 +0200

    turn test into single particle test

--- tests/test_cahn_hilliard_form.py
+++ tests/test_cahn_hilliard_form.py
@@ -9,24 +9,22 @@ import ufl
 from pyMoBiMP.cahn_hilliard_utils import cahn_hilliard_form
 
 
-def test_instantiate_two_particle_form():
+def test_instantiate_single_particle_form():
 
     # Set up the mesh
     n_elem = 128
 
     mesh = dfx.mesh.create_unit_interval(comm_world, n_elem)
 
-    n_particle = 2
-
     # The single-component element
-    elem_c = ufl.VectorElement("Lagrange", mesh.ufl_cell(), 2, n_particle)
-    elem_mu = elem_c
+    elem1 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 2)
+
+    elem_c = elem1
+    elem_mu = elem1
 
     mixed_element = elem_c * elem_mu
 
-    V = dfx.fem.FunctionSpace(
-        mesh, mixed_element
-    )  # A mixed two-component function space
+    V = dfx.fem.FunctionSpace(mesh, mixed_element)  # A mixed two-component function space
 
     u = dfx.fem.Function(V)
     u0 = dfx.fem.Function(V)
