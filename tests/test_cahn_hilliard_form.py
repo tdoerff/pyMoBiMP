060aabeb09717218010ec29ec6c634e71f21235a
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-04-04 09:44:04 +0200

    add test for single particle

--- tests/test_cahn_hilliard_form.py
+++ tests/test_cahn_hilliard_form.py
@@ -32,3 +32,30 @@ def test_instantiate_single_particle_form():
     dt = dfx.fem.Constant(mesh, 1.0)
 
     F = cahn_hilliard_form(u, u0, dt)
+
+
+def test_instantiate_two_ingle_particle_form():
+
+    # Set up the mesh
+    n_elem = 128
+
+    mesh = dfx.mesh.create_unit_interval(comm_world, n_elem)
+
+    # The single-component element
+    elem1 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 2)
+
+    elem_c = elem1
+    elem_mu = elem1
+
+    mixed_element = elem_c * elem_mu
+
+    two_particle_element = mixed_element * mixed_element
+
+    V = dfx.fem.FunctionSpace(mesh, two_particle_element)  # A mixed two-component function space
+
+    u = dfx.fem.Function(V)
+    u0 = dfx.fem.Function(V)
+
+    dt = dfx.fem.Constant(mesh, 1.0)
+
+    F = cahn_hilliard_form(u, u0, dt)
