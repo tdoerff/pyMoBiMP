dbf9a26685535f538ce41f5645e89956a67a203f
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-04-10 15:53:34 +0200

    clean up simulation script

--- experiments/CH_4_min_two_particle.py
+++ experiments/CH_4_min_two_particle.py
@@ -1,12 +1,7 @@
 # %%
 import dolfinx as dfx
 
-import h5py
-
 from dolfinx.fem.petsc import NonlinearProblem
-
-from matplotlib import pyplot as plt
-# plt.style.use('fivethirtyeight')
 
 from mpi4py import MPI
 
@@ -22,23 +17,17 @@ from pyMoBiMP.cahn_hilliard_utils import (
     AnalyzeOCP,
     y_of_c,
     c_of_y,
-    populate_initial_data)
+)
 
-from pyMoBiMP.fenicsx_utils import (evaluation_points_and_cells,
-                           get_mesh_spacing,
-                           time_stepping,
-                           NewtonSolver,
-                           FileOutput as FileOutputBase,
-                           Fenicx1DOutput)
+from pyMoBiMP.fenicsx_utils import (
+    evaluation_points_and_cells,
+    get_mesh_spacing,
+    time_stepping,
+    NewtonSolver,
+    FileOutput as FileOutputBase,
+    Fenicx1DOutput,
+)
 
-from pyMoBiMP.gmsh_utils import dfx_spherical_mesh
-
-from pyMoBiMP.plotting_utils import (
-    add_arrow,
-    plot_charging_cycle,
-    plot_time_sequence,
-    PyvistaAnimation,
-    animate_time_series)
 
 # %%
 class Output(Fenicx1DOutput):
@@ -46,8 +35,6 @@ class Output(Fenicx1DOutput):
     def extract_output(self, u_state, t):
 
         V = self.u_state.function_space
-
-        num_comp = V.num_sub_spaces
 
         output_snapshot = []
 
@@ -57,7 +44,9 @@ class Output(Fenicx1DOutput):
 
             for k_comp in range(V_sub.num_sub_spaces):
 
-                values = u_state.sub(i_comp).sub(k_comp).eval(self.x_eval, self.cells)
+                func = u_state.sub(i_comp).sub(k_comp)
+
+                values = func.eval(self.x_eval, self.cells)
 
                 output_snapshot.append(values)
 
@@ -95,7 +84,9 @@ class FileOutput(FileOutputBase):
 
         return ret
 
+
 comm_world = MPI.COMM_WORLD
+
 
 if __name__ == "__main__":
 
@@ -128,9 +119,17 @@ if __name__ == "__main__":
     num_particles = 2
 
     multi_particle_element = ufl.MixedElement(
-        [[elem_c, ] * num_particles,
-            [elem_mu, ] * num_particles]
-        )
+        [
+            [
+                elem_c,
+            ]
+            * num_particles,
+            [
+                elem_mu,
+            ]
+            * num_particles,
+        ]
+    )
 
     V = dfx.fem.FunctionSpace(mesh, multi_particle_element)
 
@@ -141,14 +140,19 @@ if __name__ == "__main__":
 
     # %%
     # Compute the chemical potential df/dc
-    a = 6. / 4
+    a = 6.0 / 4
     b = 0.2
     cc = 5
 
-    free_energy = lambda u, log, sin: u * log(u) + (1-u) * log(1-u) + a * u * (1 - u) + b * sin(cc * np.pi * u)
+    def free_energy(u, log, sin):
+        return (
+            u * log(u)
+            + (1 - u) * log(1 - u)
+            + a * u * (1 - u)
+            + b * sin(cc * np.pi * u)
+        )
 
     eps = 1e-4
-
 
     # %%
     # Experimental setup
@@ -157,25 +161,31 @@ if __name__ == "__main__":
     # charging current
     I_charge = dfx.fem.Constant(mesh, 1e-1)
 
-    T_final = 2. / I_charge.value  # ending time
+    T_final = 2.0 / I_charge.value  # ending time
 
     def experiment(t, u, I_charge, **kwargs):
 
-        return charge_discharge_stop(t, u, I_charge, c_of_y = c_of_y, **kwargs)
+        return charge_discharge_stop(t, u, I_charge, c_of_y=c_of_y, **kwargs)
 
-    event_params = dict(I_charge=I_charge, stop_on_full=False, stop_at_empty=False, cycling=False, logging=True)
+    event_params = dict(
+        I_charge=I_charge,
+        stop_on_full=False,
+        stop_at_empty=False,
+        cycling=False,
+        logging=True,
+    )
 
     # %%
     # The variational form
     # --------------------
 
     form_weights = None
-    M = lambda c: c * (1 - c)
-    theta = 1.
-    lam = 0.1
-    grad_c_bc = lambda c: 0.
 
-    alpha = 100.
+    def M(c):
+        return c * (1 - c)
+
+    alpha = 100.0
+    theta = 1.0
 
     h = ufl.Circumradius(mesh)
 
@@ -188,10 +198,7 @@ if __name__ == "__main__":
     mu1, mu2 = ufl.split(mu)
     mu01, mu02 = ufl.split(mu0)
 
-    y1, y2 = ufl.variable(y1), ufl.variable(y2)
     c1, c2 = c_of_y(y1), c_of_y(y2)
-
-    dcdy1, dcdy2 = ufl.diff(c1, y1), ufl.diff(c2, y2)
 
     v_c, v_mu = ufl.TestFunctions(V)
 
@@ -199,11 +206,15 @@ if __name__ == "__main__":
     v_mu1, v_mu2 = ufl.split(v_mu)
 
     c1, c2 = ufl.variable(c1), ufl.variable(c2)
-    f_e1, f_e2 = free_energy(c1, ufl.ln, ufl.sin), free_energy(c2, ufl.ln, ufl.sin)
+    f_e1 = free_energy(c1, ufl.ln, ufl.sin)
+    f_e2 = free_energy(c2, ufl.ln, ufl.sin)
 
     mu_chem1, mu_chem2 = ufl.diff(f_e1, c1), ufl.diff(f_e2, c2)
 
-    mu_theta1, mu_theta2 = theta * mu1 + (theta - 1.0) * mu01, theta * mu2 + (theta - 1.0) * mu02
+    mu_theta1, mu_theta2 = (
+        theta * mu1 + (theta - 1.0) * mu01,
+        theta * mu2 + (theta - 1.0) * mu02,
+    )
 
     r = ufl.SpatialCoordinate(mesh)
 
@@ -226,20 +237,36 @@ if __name__ == "__main__":
     I_charge1 = 2 * I_charge - ufl.dot(n_2, flux2)
     I_charge2 = 2 * I_charge - ufl.dot(n_1, flux1)
 
-    F1 = cahn_hilliard_form(mesh, (y1, mu1), (y01, mu01), (v_c1, v_mu1), dt,
-                            M=M, c_of_y=c_of_y,
-                            free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
-                            theta=1., lam=0.1,
-                            I_charge=I_charge1)
+    F1 = cahn_hilliard_form(
+        mesh,
+        (y1, mu1),
+        (y01, mu01),
+        (v_c1, v_mu1),
+        dt,
+        M=M,
+        c_of_y=c_of_y,
+        free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
+        theta=theta,
+        lam=0.1,
+        I_charge=I_charge1,
+    )
 
     F1 += ufl.dot(ufl.grad(v_mu1), n_1) * (mu1 - mu2) * s_A * ds
     F1 -= alpha / h * (mu1 - mu2) * v_mu1 * s_A * ds
 
-    F2 = cahn_hilliard_form(mesh, (y2, mu2), (y02, mu02), (v_c2, v_mu2), dt,
-                            M=M, c_of_y=c_of_y,
-                            free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
-                            theta=1., lam=0.1,
-                            I_charge=I_charge2)
+    F2 = cahn_hilliard_form(
+        mesh,
+        (y2, mu2),
+        (y02, mu02),
+        (v_c2, v_mu2),
+        dt,
+        M=M,
+        c_of_y=c_of_y,
+        free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
+        theta=theta,
+        lam=0.1,
+        I_charge=I_charge2,
+    )
 
     F2 += ufl.dot(ufl.grad(v_mu2), n_2) * (mu2 - mu1) * s_A * ds
     F2 -= alpha / h * (mu2 - mu1) * v_mu2 * s_A * ds
@@ -257,12 +284,16 @@ if __name__ == "__main__":
     tdim = mesh.topology.dim - 1
 
     facets = dfx.mesh.locate_entities_boundary(mesh, tdim, boundary_locator)
-    dofs1 = dfx.fem.locate_dofs_topological((V.sub(1).sub(0), V.sub(1).sub(1)), tdim, facets)
-    dofs2 = dfx.fem.locate_dofs_topological((V.sub(1).sub(1), V.sub(1).sub(0)), tdim, facets)
+    dofs1 = dfx.fem.locate_dofs_topological(
+        (V.sub(1).sub(0), V.sub(1).sub(1)), tdim, facets
+    )
+    dofs2 = dfx.fem.locate_dofs_topological(
+        (V.sub(1).sub(1), V.sub(1).sub(0)), tdim, facets
+    )
 
     bcs = [
         dfx.fem.dirichletbc(u.sub(1).sub(1), dofs1, V.sub(1).sub(0)),
-        dfx.fem.dirichletbc(u.sub(1).sub(0), dofs2, V.sub(1).sub(1))
+        dfx.fem.dirichletbc(u.sub(1).sub(0), dofs2, V.sub(1).sub(1)),
     ]
 
     # %%
@@ -272,8 +303,8 @@ if __name__ == "__main__":
     u_ini = dfx.fem.Function(V)
 
     # Constant
-    c_ini_fun = lambda x: eps * np.ones_like(x[0])
-
+    def c_ini_fun(x):
+        return eps * np.ones_like(x[0])
 
     # Store concentration-like quantity into state vector
     # ---------------------------------------------------
@@ -282,13 +313,17 @@ if __name__ == "__main__":
 
     c_ini = dfx.fem.Function(V_c)
 
-    c_ini.sub(0).interpolate(lambda x: 1. * c_ini_fun(x))
-    c_ini.sub(1).interpolate(lambda x: 10. * c_ini_fun(x))
+    c_ini.sub(0).interpolate(lambda x: 1.0 * c_ini_fun(x))
+    c_ini.sub(1).interpolate(lambda x: 10.0 * c_ini_fun(x))
 
-    y_ini1 = dfx.fem.Expression(y_of_c(c_ini.sub(0)),
-                                c_ini.sub(0).function_space.element.interpolation_points())
-    y_ini2 = dfx.fem.Expression(y_of_c(c_ini.sub(1)),
-                                c_ini.sub(1).function_space.element.interpolation_points())
+    y_ini1 = dfx.fem.Expression(
+        y_of_c(c_ini.sub(0)),
+        c_ini.sub(0).function_space.element.interpolation_points()
+    )
+    y_ini2 = dfx.fem.Expression(
+        y_of_c(c_ini.sub(1)),
+        c_ini.sub(1).function_space.element.interpolation_points()
+    )
 
     u_ini.sub(0).sub(0).interpolate(y_ini1)
     u_ini.sub(0).sub(1).interpolate(y_ini2)
@@ -300,13 +335,17 @@ if __name__ == "__main__":
     dFdc1 = ufl.diff(free_energy(c_ini1, ufl.ln, ufl.sin), c_ini1)
 
     W = u_ini.sub(1).sub(0).function_space
-    u_ini.sub(1).sub(0).interpolate(dfx.fem.Expression(dFdc1, W.element.interpolation_points()))
+    u_ini.sub(1).sub(0).interpolate(
+        dfx.fem.Expression(dFdc1, W.element.interpolation_points())
+    )
 
     c_ini2 = ufl.variable(c_ini.sub(1))
     dFdc2 = ufl.diff(free_energy(c_ini2, ufl.ln, ufl.sin), c_ini2)
 
     W = u_ini.sub(1).sub(1).function_space
-    u_ini.sub(1).sub(1).interpolate(dfx.fem.Expression(dFdc2, W.element.interpolation_points()))
+    u_ini.sub(1).sub(1).interpolate(
+        dfx.fem.Expression(dFdc2, W.element.interpolation_points())
+    )
 
     u_ini.x.scatter_forward()
 
@@ -332,7 +371,9 @@ if __name__ == "__main__":
 
     output_xdmf = FileOutput(u, np.linspace(0, T_final, 51), filename=filename)
 
-    rt_analysis = AnalyzeOCP(c_of_y=c_of_y, filename=results_folder / "CH_4_min_1D_rt.txt")
+    rt_analysis = AnalyzeOCP(
+        c_of_y=c_of_y, filename=results_folder / "CH_4_min_1D_rt.txt"
+    )
 
     # %%
     # Run the experiment
