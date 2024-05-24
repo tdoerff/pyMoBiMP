9c772451437a4778e4acb067bfacb9575fc1f956
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-05-24 11:43:08 +0200

    only encapsulate time stepping

--- experiments/CH_4_min_two_particle.py
+++ experiments/CH_4_min_two_particle.py
@@ -159,323 +159,323 @@ class AnalyzeCellPotential(RuntimeAnalysisBase):
 comm_world = MPI.COMM_WORLD
 
 
-if __name__ == "__main__":
+# %%
+# Discretization
+# --------------
 
-    # %%
-    # Discretization
-    # --------------
+# Set up the mesh
+n_elem = 32
 
-    # Set up the mesh
-    n_elem = 32
+mesh_filename = "Meshes/line_mesh.xdmf"
 
-    mesh_filename = "Meshes/line_mesh.xdmf"
+if os.path.isfile(mesh_filename):
+    # Load mesh from file
+    with dfx.io.XDMFFile(comm_world, mesh_filename, 'r') as file:
+        mesh = file.read_mesh(name="Grid")
+else:
+    mesh = dfx.mesh.create_unit_interval(comm_world, n_elem)
 
-    if os.path.isfile(mesh_filename):
-        # Load mesh from file
-        with dfx.io.XDMFFile(comm_world, mesh_filename, 'r') as file:
-            mesh = file.read_mesh(name="Grid")
-    else:
-        mesh = dfx.mesh.create_unit_interval(comm_world, n_elem)
+dx_cell = get_mesh_spacing(mesh)
 
-    dx_cell = get_mesh_spacing(mesh)
+print(f"Cell spacing: h = {dx_cell}")
 
-    print(f"Cell spacing: h = {dx_cell}")
+# For later plotting use
+x = np.linspace(0, 1, 101)
+points_on_proc, cells = evaluation_points_and_cells(mesh, x)
 
-    # For later plotting use
-    x = np.linspace(0, 1, 101)
-    points_on_proc, cells = evaluation_points_and_cells(mesh, x)
+# Initial timestep size
+dt = dfx.fem.Constant(mesh, dx_cell * 0.01)
 
-    # Initial timestep size
-    dt = dfx.fem.Constant(mesh, dx_cell * 0.01)
+# %%
+elem1 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
 
-    # %%
-    elem1 = ufl.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+elem_c = elem1
+elem_mu = elem1
 
-    elem_c = elem1
-    elem_mu = elem1
+num_particles = 10
 
-    num_particles = 10
-
-    multi_particle_element = ufl.MixedElement(
+multi_particle_element = ufl.MixedElement(
+    [
         [
-            [
-                elem_c,
-            ]
-            * num_particles,
-            [
-                elem_mu,
-            ]
-            * num_particles,
+            elem_c,
         ]
+        * num_particles,
+        [
+            elem_mu,
+        ]
+        * num_particles,
+    ]
+)
+
+V = dfx.fem.FunctionSpace(mesh, multi_particle_element)
+
+# %%
+# The mixed-element functions
+u = dfx.fem.Function(V)
+u0 = dfx.fem.Function(V)
+
+# %%
+# Compute the chemical potential df/dc
+a = 6.0 / 4
+b = 0.2
+cc = 5
+
+def free_energy(u, log, sin):
+    return (
+        u * log(u)
+        + (1 - u) * log(1 - u)
+        + a * u * (1 - u)
+        + b * sin(cc * np.pi * u)
     )
 
-    V = dfx.fem.FunctionSpace(mesh, multi_particle_element)
+eps = 1e-2
 
-    # %%
-    # The mixed-element functions
-    u = dfx.fem.Function(V)
-    u0 = dfx.fem.Function(V)
+res_c_left = sp.optimize.minimize_scalar(
+    lambda c: free_energy(c, np.log, np.sin),
+    bracket=(2 * eps, 0.05),
+    bounds=(eps, 0.05))
 
-    # %%
-    # Compute the chemical potential df/dc
-    a = 6.0 / 4
-    b = 0.2
-    cc = 5
+assert res_c_left.success
+c_left = res_c_left.x
 
-    def free_energy(u, log, sin):
-        return (
-            u * log(u)
-            + (1 - u) * log(1 - u)
-            + a * u * (1 - u)
-            + b * sin(cc * np.pi * u)
+res_c_right = sp.optimize.minimize_scalar(
+    lambda c: free_energy(c, np.log, np.sin),
+    bracket=(0.95, 1 - 2 * eps),
+    bounds=(0.95, 1 - eps))
+
+assert res_c_right.success
+c_right = res_c_right.x
+
+# %%
+# Experimental setup
+# ------------------
+
+#
+C_rate = 0.01
+
+# charging current
+I_charge = dfx.fem.Constant(mesh, 1. / 3. * C_rate * num_particles)
+
+T_final = 6.0 / C_rate if C_rate > 0 else 2.0  # ending time
+
+def experiment(
+    t,
+    u,
+    I_charge,
+    c_bounds=[0.05, 0.99],
+    c_of_y=c_of_y,
+    stop_at_empty=True,
+    stop_on_full=True,
+    cycling=True,
+    logging=False,
+):
+
+    coords = ufl.SpatialCoordinate(u.function_space.mesh)
+    r = ufl.sqrt(sum([c**2 for c in coords]))
+
+    y, _ = u.split()
+
+    cs = [c_of_y(y_) for y_ in y]
+
+    # This is a bit hackish, since we just need to multiply by a function that
+    # is zero at r=0 and 1 at r=1.
+    cs_bc = [dfx.fem.form(r**2 * c_ * ufl.ds) for c_ in cs]
+    cs_bc = [dfx.fem.assemble_scalar(c_bc) for c_bc in cs_bc]
+
+    if logging:
+        print(f"t={t:1.5f} ; c_bc = [{min(cs_bc):1.3e}, {max(cs_bc):1.3e}]", c_bounds)
+
+    if max(cs_bc) > c_bounds[1] and I_charge.value > 0.0:
+        print(
+            ">>> charge at boundary exceeds maximum " +
+            f"(max(c) = {max(cs_bc):1.3f} > {c_bounds[1]:1.3f})."
         )
 
-    eps = 1e-2
+        if stop_on_full:
+            print(">>> Particle is filled.")
 
-    res_c_left = sp.optimize.minimize_scalar(
-        lambda c: free_energy(c, np.log, np.sin),
-        bracket=(2 * eps, 0.05),
-        bounds=(eps, 0.05))
+            return True
 
-    assert res_c_left.success
-    c_left = res_c_left.x
-
-    res_c_right = sp.optimize.minimize_scalar(
-        lambda c: free_energy(c, np.log, np.sin),
-        bracket=(0.95, 1 - 2 * eps),
-        bounds=(0.95, 1 - eps))
-
-    assert res_c_right.success
-    c_right = res_c_right.x
-
-    # %%
-    # Experimental setup
-    # ------------------
-
-    #
-    C_rate = 0.01
-
-    # charging current
-    I_charge = dfx.fem.Constant(mesh, 1. / 3. * C_rate * num_particles)
-
-    T_final = 6.0 / C_rate if C_rate > 0 else 2.0  # ending time
-
-    def experiment(
-        t,
-        u,
-        I_charge,
-        c_bounds=[0.05, 0.99],
-        c_of_y=c_of_y,
-        stop_at_empty=True,
-        stop_on_full=True,
-        cycling=True,
-        logging=False,
-    ):
-
-        coords = ufl.SpatialCoordinate(u.function_space.mesh)
-        r = ufl.sqrt(sum([c**2 for c in coords]))
-
-        y, _ = u.split()
-
-        cs = [c_of_y(y_) for y_ in y]
-
-        # This is a bit hackish, since we just need to multiply by a function that
-        # is zero at r=0 and 1 at r=1.
-        cs_bc = [dfx.fem.form(r**2 * c_ * ufl.ds) for c_ in cs]
-        cs_bc = [dfx.fem.assemble_scalar(c_bc) for c_bc in cs_bc]
-
-        if logging:
-            print(f"t={t:1.5f} ; c_bc = [{min(cs_bc):1.3e}, {max(cs_bc):1.3e}]", c_bounds)
-
-        if max(cs_bc) > c_bounds[1] and I_charge.value > 0.0:
-            print(
-                ">>> charge at boundary exceeds maximum " +
-                f"(max(c) = {max(cs_bc):1.3f} > {c_bounds[1]:1.3f})."
-            )
-
-            if stop_on_full:
-                print(">>> Particle is filled.")
-
-                return True
-
-            print(">>> Start discharging.")
-            I_charge.value *= -1.0
-
-            return False
-
-        if min(cs_bc) < c_bounds[0] and I_charge.value < 0.0:
-
-            if stop_at_empty:
-                print(">>> Particle is emptied!")
-
-                return True
-
-            else:
-                if cycling:
-                    print(">>> Start charging.")
-                    I_charge.value *= -1.0
-
-                else:
-                    print(">>> Stop charging.")
-                    I_charge.value = 0.0
-
-                return False
+        print(">>> Start discharging.")
+        I_charge.value *= -1.0
 
         return False
 
-    event_params = dict(
-        I_charge=I_charge,
-        stop_on_full=False,
-        stop_at_empty=False,
-        cycling=False,
-        logging=True,
+    if min(cs_bc) < c_bounds[0] and I_charge.value < 0.0:
+
+        if stop_at_empty:
+            print(">>> Particle is emptied!")
+
+            return True
+
+        else:
+            if cycling:
+                print(">>> Start charging.")
+                I_charge.value *= -1.0
+
+            else:
+                print(">>> Stop charging.")
+                I_charge.value = 0.0
+
+            return False
+
+    return False
+
+event_params = dict(
+    I_charge=I_charge,
+    stop_on_full=False,
+    stop_at_empty=False,
+    cycling=False,
+    logging=True,
+)
+
+# %%
+# The variational form
+# --------------------
+
+def M(c):
+    return c * (1 - c)
+
+theta = 1.0
+
+y, mu = ufl.split(u)
+y0, mu0 = ufl.split(u0)
+
+y1s = ufl.split(y)
+y0s = ufl.split(y0)
+
+mu1s = ufl.split(mu)
+mu0s = ufl.split(mu0)
+
+v_c, v_mu = ufl.TestFunctions(V)
+
+v_cs = ufl.split(v_c)
+v_mus = ufl.split(v_mu)
+
+mu_theta = [theta * mu1s_ + (theta - 1.0) * mu0s_
+            for mu1s_, mu0s_ in zip(mu1s, mu0s)]
+
+# particle parameters
+Rs = np.ones(num_particles)
+
+As = 4 * np.pi * Rs
+
+A = sum(As)
+
+# fraction of the total surface
+a_ratios = As / A
+
+# Coupling parameters between particle surface potential.
+Ls = 1.e1 * (1 + 0.1 * (2 * np.random.random(num_particles) - 1))
+L = sum([a_ * L_ for a_, L_ in zip(a_ratios, Ls)])
+
+# I * (A_1 + A_2) = I_1 * A_1 + I_2 * A_2
+term = sum([L_ * a_ * mu_ for L_, a_, mu_ in zip(Ls, a_ratios, mu_theta)])
+
+# Here must ne a negative sign since with the I_charges, we measure
+# what flows out of the particle.
+Voltage = - I_charge / L - term / L
+
+# TODO: Check the sign! Somehow, there must be a minus for the
+# code to work. I think, I_charge as constructed here is the current
+# OUT OF the particle.
+I_charges = [
+    -L_ * (mu_ + Voltage) / A_ for L_, mu_, A_ in zip(Ls, mu_theta, As)]
+
+# Assemble the individual particle forms.
+Fs = [
+    cahn_hilliard_form(
+        mesh,
+        (y1_, mu1_),
+        (y0_, mu0_),
+        (v_c_, v_mu_),
+        dt,
+        M=M,
+        c_of_y=c_of_y,
+        free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
+        theta=theta,
+        lam=0.1,
+        I_charge=I_charge_,
+    ) for y1_, mu1_, y0_, mu0_, v_c_, v_mu_, I_charge_ in zip(
+        y1s, mu1s, y0s, mu0s, v_cs, v_mus, I_charges
+    )
+    ]
+
+# Compose the global FEM form.
+F = sum(Fs)
+
+# %%
+# Initial data
+# ------------
+
+u_ini = dfx.fem.Function(V)
+
+# Constant
+def c_ini_fun(x):
+    return eps * np.ones_like(x[0])
+
+# Store concentration-like quantity into state vector
+# ---------------------------------------------------
+
+V_c, _ = V.sub(0).collapse()
+
+c_ini = dfx.fem.Function(V_c)
+
+for i_particle in range(num_particles):
+
+    c_ini.sub(i_particle).interpolate(lambda x: c_left + 0 * c_ini_fun(x))
+
+    W = c_ini.sub(i_particle).function_space
+    x_interpolate = W.element.interpolation_points()
+
+    y_ini = dfx.fem.Expression(
+        y_of_c(c_ini.sub(i_particle)), x_interpolate)
+
+    u_ini.sub(0).sub(i_particle).interpolate(y_ini)
+
+# Store chemical potential into state vector
+# ------------------------------------------
+
+for i_particle in range(num_particles):
+    c_ini_ = ufl.variable(c_ini.sub(i_particle))
+    dFdc1 = ufl.diff(free_energy(c_ini_, ufl.ln, ufl.sin), c_ini_)
+
+    W = u_ini.sub(1).sub(i_particle).function_space
+    u_ini.sub(1).sub(i_particle).interpolate(
+        dfx.fem.Expression(dFdc1, W.element.interpolation_points())
     )
 
-    # %%
-    # The variational form
-    # --------------------
+u_ini.x.scatter_forward()
 
-    def M(c):
-        return c * (1 - c)
+# %%
+problem = NonlinearProblem(F, u)
 
-    theta = 1.0
+solver = NewtonSolver(comm_world, problem)
+# %%
+# Set up experiment
+# -----------------
 
-    y, mu = ufl.split(u)
-    y0, mu0 = ufl.split(u0)
+u.interpolate(u_ini)
 
-    y1s = ufl.split(y)
-    y0s = ufl.split(y0)
+n_out = 501
 
-    mu1s = ufl.split(mu)
-    mu0s = ufl.split(mu0)
+results_folder = Path("simulation_output")
+results_folder.mkdir(exist_ok=True, parents=True)
 
-    v_c, v_mu = ufl.TestFunctions(V)
+base_filename = "CH_4_min_2_particle"
 
-    v_cs = ufl.split(v_c)
-    v_mus = ufl.split(v_mu)
+filename = results_folder / (base_filename + ".xdmf")
 
-    mu_theta = [theta * mu1s_ + (theta - 1.0) * mu0s_
-                for mu1s_, mu0s_ in zip(mu1s, mu0s)]
+output_xdmf = FileOutput(
+    u, np.linspace(0, T_final, n_out), filename=filename)
 
-    # particle parameters
-    Rs = np.ones(num_particles)
+rt_analysis = AnalyzeCellPotential(
+    Ls, As, I_charge,
+    c_of_y=c_of_y, filename=results_folder / (base_filename + "_rt.txt")
+)
 
-    As = 4 * np.pi * Rs
 
-    A = sum(As)
-
-    # fraction of the total surface
-    a_ratios = As / A
-
-    # Coupling parameters between particle surface potential.
-    Ls = 1.e1 * (1 + 0.1 * (2 * np.random.random(num_particles) - 1))
-    L = sum([a_ * L_ for a_, L_ in zip(a_ratios, Ls)])
-
-    # I * (A_1 + A_2) = I_1 * A_1 + I_2 * A_2
-    term = sum([L_ * a_ * mu_ for L_, a_, mu_ in zip(Ls, a_ratios, mu_theta)])
-
-    # Here must ne a negative sign since with the I_charges, we measure
-    # what flows out of the particle.
-    Voltage = - I_charge / L - term / L
-
-    # TODO: Check the sign! Somehow, there must be a minus for the
-    # code to work. I think, I_charge as constructed here is the current
-    # OUT OF the particle.
-    I_charges = [
-        -L_ * (mu_ + Voltage) / A_ for L_, mu_, A_ in zip(Ls, mu_theta, As)]
-
-    # Assemble the individual particle forms.
-    Fs = [
-        cahn_hilliard_form(
-            mesh,
-            (y1_, mu1_),
-            (y0_, mu0_),
-            (v_c_, v_mu_),
-            dt,
-            M=M,
-            c_of_y=c_of_y,
-            free_energy=lambda c: free_energy(c, ufl.ln, ufl.sin),
-            theta=theta,
-            lam=0.1,
-            I_charge=I_charge_,
-        ) for y1_, mu1_, y0_, mu0_, v_c_, v_mu_, I_charge_ in zip(
-            y1s, mu1s, y0s, mu0s, v_cs, v_mus, I_charges
-        )
-        ]
-
-    # Compose the global FEM form.
-    F = sum(Fs)
-
-    # %%
-    # Initial data
-    # ------------
-
-    u_ini = dfx.fem.Function(V)
-
-    # Constant
-    def c_ini_fun(x):
-        return eps * np.ones_like(x[0])
-
-    # Store concentration-like quantity into state vector
-    # ---------------------------------------------------
-
-    V_c, _ = V.sub(0).collapse()
-
-    c_ini = dfx.fem.Function(V_c)
-
-    for i_particle in range(num_particles):
-
-        c_ini.sub(i_particle).interpolate(lambda x: c_left + 0 * c_ini_fun(x))
-
-        W = c_ini.sub(i_particle).function_space
-        x_interpolate = W.element.interpolation_points()
-
-        y_ini = dfx.fem.Expression(
-            y_of_c(c_ini.sub(i_particle)), x_interpolate)
-
-        u_ini.sub(0).sub(i_particle).interpolate(y_ini)
-
-    # Store chemical potential into state vector
-    # ------------------------------------------
-
-    for i_particle in range(num_particles):
-        c_ini_ = ufl.variable(c_ini.sub(i_particle))
-        dFdc1 = ufl.diff(free_energy(c_ini_, ufl.ln, ufl.sin), c_ini_)
-
-        W = u_ini.sub(1).sub(i_particle).function_space
-        u_ini.sub(1).sub(i_particle).interpolate(
-            dfx.fem.Expression(dFdc1, W.element.interpolation_points())
-        )
-
-    u_ini.x.scatter_forward()
-
-    # %%
-    problem = NonlinearProblem(F, u)
-
-    solver = NewtonSolver(comm_world, problem)
-    # %%
-    # Set up experiment
-    # -----------------
-
-    u.interpolate(u_ini)
-
-    n_out = 501
-
-    results_folder = Path("simulation_output")
-    results_folder.mkdir(exist_ok=True, parents=True)
-
-    base_filename = "CH_4_min_2_particle"
-
-    filename = results_folder / (base_filename + ".xdmf")
-
-    output_xdmf = FileOutput(
-        u, np.linspace(0, T_final, n_out), filename=filename)
-
-    rt_analysis = AnalyzeCellPotential(
-        Ls, As, I_charge,
-        c_of_y=c_of_y, filename=results_folder / (base_filename + "_rt.txt")
-    )
-
+if __name__ == "__main__":
     # %%
     # Run the experiment
     # ------------------
