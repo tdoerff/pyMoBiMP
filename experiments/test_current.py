012521c863c50cbf5fbb71b7b6d3f35691076f4a
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-06-06 16:55:05 +0200

    update test

--- experiments/test_current.py
+++ experiments/test_current.py
@@ -5,22 +5,25 @@ import pytest
 from CH_4_min_n_particles import compute_particle_current_densities
 
 
-def test_test():
-
-    num_particles = 2
+@pytest.mark.parametrize("num_particles", [1, 2, 5])
+def test_current(num_particles):
 
     mus = 3 * (2 * np.random.random(num_particles) - 1)
-    As = 4 * np.pi * np.ones(num_particles)
+    As = 4 * np.pi * np.random.random(num_particles)
     Ls = 1e3 * (2 * np.random.random(num_particles) - 1)
-    I_charge = 1.0
+    i_charge = 1.0
 
     i_charges = compute_particle_current_densities(
-        mus, As, Ls, I_charge
+        mus, As, Ls, i_charge
     )
 
     A = sum(As)
 
     aas = As / A
 
-    assert np.isclose(I_charge,
-                      sum([a_ * i_ for a_, i_ in zip(aas, i_charges)]))
+    # Check if the sum over the individual particle currents matches
+    # the total current.
+
+    I_charge_sum = sum([a_ * i_ for a_, i_ in zip(aas, i_charges)])
+
+    assert np.isclose(i_charge, I_charge_sum)
