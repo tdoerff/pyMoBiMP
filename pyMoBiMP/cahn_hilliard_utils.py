a06f6ea80c65505c79019a5e9d3041e4d9cc7e8f
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-06-24 15:39:03 +0200

    put code for both forms into cahn_hilliard_utils

--- pyMoBiMP/cahn_hilliard_utils.py
+++ pyMoBiMP/cahn_hilliard_utils.py
@@ -27,6 +27,24 @@ from .fenicsx_utils import (
 )
 
 
+# Defaults for the Simulation class
+_mesh = dfx.mesh.create_unit_interval(MPI.COMM_WORLD, 128)
+
+
+def _free_energy(
+    u: dfx.fem.Function, a: float = 6.0 / 4.0, b: float = 0.2, c: float = 5.0
+):
+
+    fe = (
+        u * ufl.ln(u)
+        + (1 - u) * ufl.ln(1 - u)
+        + a * u * (1 - u)
+        + b * ufl.sin(c * np.pi * u)
+    )
+
+    return fe
+
+
 # Forward and backward variable transformation.
 def c_of_y(y):
     return ufl.exp(y) / (1 + ufl.exp(y))
@@ -204,6 +222,78 @@ def compute_dcdy(y, c_of_y):
     return c, dcdy
 
 
+def cahn_hilliard_mu_form(
+        y,
+        c_of_y=c_of_y,
+        free_energy=_free_energy,
+        gamma=0.1,
+        form_weights=None):
+
+    V = y.function_space
+    mesh = V.mesh
+
+    r = ufl.SpatialCoordinate(mesh)
+
+    # adaptation of the volume element due to geometry
+    if form_weights is not None:
+        s_V = form_weights["volume"]
+    else:
+        s_V = 4 * np.pi * r**2
+
+    c = c_of_y(y)
+    mu_chem = compute_chemical_potential(free_energy, c)
+
+    # adaptation of the volume element due to geometry
+    if form_weights is not None:
+        s_V = form_weights["volume"]
+    else:
+        s_V = 4 * np.pi * r**2
+
+    mu_ = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+
+    residual_mu = s_V * ufl.inner(mu_, v) * ufl.dx
+    residual_mu -= s_V * ufl.inner(v, mu_chem) * ufl.dx + \
+        gamma * s_V * ufl.inner(ufl.grad(c), ufl.grad(v)) * ufl.dx
+
+    return residual_mu
+
+
+def cahn_hilliard_dydt_form(
+        y,
+        mu,
+        I_charge,
+        M=lambda c: c * (1 - c),
+        c_of_y=c_of_y,
+        form_weights=None):
+
+    V = mu.function_space
+    mesh = V.mesh
+
+    r = ufl.SpatialCoordinate(mesh)
+
+    # adaptation of the volume element due to geometry
+    if form_weights is not None:
+        s_V = form_weights["volume"]
+        s_A = form_weights["surface"]
+    else:
+        s_V = 4 * np.pi * r**2
+        s_A = 2 * np.pi * r**2
+
+    c, dcdy = compute_dcdy(y, c_of_y)
+
+    flux = M(c) * ufl.grad(mu)
+
+    dydt = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+
+    residual_dydt = s_V * dcdy * dydt * v * ufl.dx
+    residual_dydt -= s_A * I_charge * v * ufl.ds
+    residual_dydt -= -s_V * ufl.dot(ufl.grad(v), flux) * ufl.dx
+
+    return residual_dydt
+
+
 def populate_initial_data(u_ini, c_ini_fun, free_energy, y_of_c=y_of_c):
 
     # Store concentration-like quantity into state vector
@@ -340,23 +430,6 @@ class AnalyzeOCP(RuntimeAnalysisBase):
 
         return super().analyze(u_state, t)
 
-
-# Defaults for the Simulation class
-_mesh = dfx.mesh.create_unit_interval(MPI.COMM_WORLD, 128)
-
-
-def _free_energy(
-    u: dfx.fem.Function, a: float = 6.0 / 4.0, b: float = 0.2, c: float = 5.0
-):
-
-    fe = (
-        u * ufl.ln(u)
-        + (1 - u) * ufl.ln(1 - u)
-        + a * u * (1 - u)
-        + b * ufl.sin(c * np.pi * u)
-    )
-
-    return fe
 
 
 class Simulation:
