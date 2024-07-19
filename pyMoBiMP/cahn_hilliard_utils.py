6458cbf6d0604ff83a20d1683748071bc912663c
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-07-19 11:10:08 +0200

    remove useless setter

--- pyMoBiMP/cahn_hilliard_utils.py
+++ pyMoBiMP/cahn_hilliard_utils.py
@@ -908,10 +908,6 @@ class MultiParticleSimulation():
         self._T = 6.0 / self.C_rate if self.C_rate > 0 else 2.0  # ending time
         return self._T
 
-    @T_final.setter
-    def T_final(self, value):
-        self._T = value
-
     @staticmethod
     def free_energy(u, log, sin):
         a = 6.0 / 4
