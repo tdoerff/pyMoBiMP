79cdc87ad125ca1f0e5ccdfc2602e4bd6b821f0d
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-07-25 12:57:10 +0200

    make sure we really set gamma

--- experiments/parameter_sweeps/gamma/1.0/experiment.py
+++ experiments/parameter_sweeps/gamma/1.0/experiment.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     simulation = Simulation(
         mesh,
         output_destination=exp_path + "/simulation_output/output",
-        lam=1.)
+        gamma=1.)
 
     simulation.run(tol=1e-5)
--- experiments/parameter_sweeps/gamma/10./experiment.py
+++ experiments/parameter_sweeps/gamma/10./experiment.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     simulation = Simulation(
         mesh,
         output_destination=exp_path + "/simulation_output/output",
-        lam=10.)
+        gamma=10.)
 
     simulation.run(tol=1e-5)
