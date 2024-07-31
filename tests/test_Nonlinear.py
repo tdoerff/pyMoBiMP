c6c9755fdc7ffc6dec2458ed366adba9c5f72644
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-07-31 19:12:51 +0200

    add test(failing)

--- tests/test_Nonlinear.py
+++ tests/test_Nonlinear.py
@@ -12,6 +12,7 @@ import pytest
 import ufl
 
 from pyMoBiMP.fenicsx_utils import (
+    evaluation_points_and_cells,
     NewtonSolver,
     NonlinearProblem,
     BlockNewtonSolver,
@@ -226,3 +227,91 @@ def test_nonlinear_block_algebraic():
         L2_err0 = mesh.comm.allreduce(L2_err0_loc, op=SUM)
 
         assert np.isclose(L2_err0, 0.)
+
+
+def test_nonlinear_block_differential():
+
+    # Adjacent meshes
+    meshes = [dfx.mesh.create_interval(comm, 128, [-1, 0.]),
+              dfx.mesh.create_unit_interval(comm, 128)]
+
+    def set_up_u(mesh):
+        V = dfx.fem.FunctionSpace(mesh, ("Lagrange", 4))
+
+        u = dfx.fem.Function(V)
+
+        return u
+
+    us = [set_up_u(mesh) for mesh in meshes]
+
+    def set_up_form(u):
+
+        V = u.function_space
+        mesh = V.mesh
+
+        x_min = mesh.geometry.x[:, 0].min()
+        x_max = mesh.geometry.x[:, 0].max()
+
+        u_left = dfx.fem.Constant(mesh, 0.,)
+        u_right = dfx.fem.Constant(mesh, 0.,)
+
+        dofs_left = dfx.fem.locate_dofs_geometrical(
+            V, lambda x: np.isclose(x[0], x_min))
+
+        dofs_right = dfx.fem.locate_dofs_geometrical(
+            V, lambda x: np.isclose(x[0], x_max))
+
+        bcs = [dfx.fem.dirichletbc(u_left, dofs_left, V),
+               dfx.fem.dirichletbc(u_right, dofs_right, V)]
+
+        v = ufl.TestFunction(V)
+        F = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx - 2 * v * ufl.dx
+
+        return F, u, bcs, u_left, u_right
+
+    problem_defs = [set_up_form(u) for u in us]
+
+    Fs = [F for F, *_ in problem_defs]
+    us = [u for _, u, *_ in problem_defs]
+    bcs = [bc for _, _, bc, *_ in problem_defs]
+    u_lefts = [u_left for _, _, _, u_left, _ in problem_defs]
+    u_rights = [u_right for _, _, _, _, u_right in problem_defs]
+
+    # Manually set the boundary conditions for both grids.
+    u_lefts[0].value = 2.
+    u_rights[0].value = 1.
+    u_lefts[1].value = 1.
+    u_rights[1].value = 2.
+
+    problem = BlockNonlinearProblem(Fs, us, bcs)
+
+    coords = ufl.SpatialCoordinate(meshes[1])
+    r2 = ufl.dot(coords, coords)
+
+    # The term (1 - r2) selects the inner interface.
+    u_1_l_form = dfx.fem.form(us[1] * (1 - r2) * ufl.ds)
+    u_0_r_form = dfx.fem.form(us[0] * (1 - r2) * ufl.ds)
+
+    def callback(solver, uhs):
+        """
+        Assign the boundary inner value of the left domain to right one
+        and vice versa.
+        """
+
+        # First function
+        # ---------------
+        u_1_l = dfx.fem.assemble_scalar(u_1_l_form)
+
+        u_rights[0].value = u_1_l
+
+        # # Second function
+        # # ---------------
+        u_0_r = dfx.fem.assemble_scalar(u_0_r_form)
+
+        u_lefts[1].value = u_0_r
+
+    solver = BlockNewtonSolver(comm, problem, callback=callback)
+
+    solver.solve(us)
+
+    assert True
