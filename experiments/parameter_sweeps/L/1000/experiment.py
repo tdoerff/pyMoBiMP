c40e8db5844c1ade1e788e7d9ec355dedcee097d
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-08-07 10:45:15 +0200

    add experiments on L parameter

---
+++ experiments/parameter_sweeps/L/1000/experiment.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+
+exp_path = os.path.dirname(__file__)
+parent_path = os.path.abspath(
+    exp_path + "/../..")
+
+sys.path.append(parent_path)
+
+from default.experiment import Simulation as SimulationBase, mesh  # noqa: 402
+
+
+class Simulation(SimulationBase):
+
+    @classmethod
+    def experiment(cls, *args, c_bounds=[-5., 5], **kwargs):
+
+        super().experiment(*args, c_bounds=c_bounds, **kwargs)
+
+
+if __name__ == "__main__":
+
+    L_mean = 1000.
+
+    simulation = Simulation(
+        mesh,
+        L_mean=L_mean,
+        output_destination=exp_path + "/simulation_output/output")
+
+    simulation.run(tol=1e-5)
