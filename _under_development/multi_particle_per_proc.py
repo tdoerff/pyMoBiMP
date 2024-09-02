a8fb5d83221e76e10868c7440c16acce1fc23bab
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-09-02 10:46:51 +0200

    WIP: commit everything so far from multi_particle_per_proc

--- _under_development/multi_particle_per_proc.py
+++ _under_development/multi_particle_per_proc.py
@@ -39,7 +39,7 @@ class Simulation(SimulationBase):
 
     def run(self,
             dt_max=1e-1,
-            dt_min=1e-8,
+            dt_min=1e-9,
             tol=1e-4,
             dt_increase=1.1):
 
@@ -71,9 +71,13 @@ class Simulation(SimulationBase):
 
         facet_tag = dfx.mesh.meshtags(self.mesh, fdim, facets, facet_markers)
 
+        # The surface measure at the outer radius.
         dA = ufl.Measure("ds", domain=mesh, subdomain_data=facet_tag)
+        dA = dA(1)  # This is important to get the measure at the outer radius.
 
+        # The total particle surface within the cell ...
         A_p = sum(self.As)
+        # ... and the particle surface ratios.
         aas = self.As / A_p
 
         # The total particle surface across all cells
@@ -82,37 +86,53 @@ class Simulation(SimulationBase):
         # Ratio between cell surface and total surface
         a_p = A_p / A
 
+        # inter-cell weighted average of affinity parameter
         L_p = sum(aas * self.Ls)
 
         L = self.comm.allreduce(L_p * a_p)
 
+        # List of mus within cell
         mus = self.u.sub(1).split()
 
+        # The ufl form of the OCP of the cell ...
         V_p_OCP_form_ufl = - 1 / L_p * sum(
             [L_ * a_ * mu_ * dA for L_, a_, mu_ in zip(self.Ls, aas, mus)])
-
+        # ... and the compiled form.
         V_p_OCP_form = dfx.fem.form(V_p_OCP_form_ufl)
 
+        # Outer timestepping loop
         while t < self.T_final:
 
+            # Check if timestep is not too small, abort otherwise.
+            # (Since the dt.value is synchronized across all processors,
+            # all of them will abort simulaneously.)
+            if float(dt) < dt_min:
+                raise ValueError(f"Timestep too small (dt={dt.value})!")
+
+            # Increase the timestep counter
             it += 1
 
             # if self.rt_analysis is not None:
             #     self.rt_analysis.analyze(t)
 
+            # Set up the old time step with previous solution
             self.u.x.scatter_forward()
             self.u0.x.array[:] = self.u.x.array[:]
             self.u0.x.scatter_forward()
 
+            # Parameters for the inner loop iterating over the nonlinear
+            # battery voltage.
             tol = 1e-6
-            voltage_err = 1e99
+            voltage_inc = 1e99
             voltage_old = 0.
             it_voltage = 0
-            max_it_voltage = 100
+            max_it_voltage = 20  # we shouldn't really need more than that!
 
-            iterations = 0  # counter for the inner iterations
+            # Keep track of number of Newton iterations. This number will
+            # be accumulated across all processes.
+            newton_iterations = 0
 
-            while (voltage_err > tol) and (it_voltage < max_it_voltage):
+            while (voltage_inc > tol) and (it_voltage < max_it_voltage):
 
                 it_voltage += 1
 
@@ -131,86 +151,84 @@ class Simulation(SimulationBase):
                 voltage_inc = abs(voltage - voltage_old)
                 voltage_old = voltage
 
+                # Call experiment (updates I_battery)
+                stop = self.experiment(t, cell_voltage=voltage)
+
                 # Update the cell current according to global voltage
                 self.I_charge.value = L_p * (V_p_OCP - voltage)
-
-                stop = self.experiment(t, cell_voltage=voltage)
 
                 if stop:
                     break
 
+                # Perform the time step.
+                # ----------------------
                 try:
-                    if float(dt) < dt_min:
+                    it_k, success = self.solver.solve(self.u)
+                except Exception as e:
+                    # If anything goes wrong here, mark as unsuccessfull
+                    log(e)
+                    it_k = 9999
+                    success = False
 
-                        raise ValueError(f"Timestep too small (dt={dt.value})!")
+                # All processes should arrive here even in case of a crash
+                # that may have happened on one core.
+                successes = self.comm.allgather(success)
 
-                    try:
-                        it_k, success = self.solver.solve(self.u)
-                    except Exception as e:
-                        log(e)
-                        it_k = 9999
+                # True when all processes survived.
+                success = np.all(successes)
 
-                    self.comm.barrier()
-                    # log("Hit barrier")
+                # Raise exception if one or more processes crashed.
+                if not success:
+                    log("One or more solvers did not converge.",
+                        cond=(self.comm.rank == 0))
 
-                    iterations = max(it_k, iterations)
+                    # Jump out of voltage iteration loop (with success==False)
+                    break
 
-                    if not success:
-                        raise RuntimeError("Newton solver did not converge.")
-                    else:
-                        iterations = MPI.COMM_WORLD.allreduce(
-                            iterations, op=MPI.MAX)
+                else:
+                    it_k = MPI.COMM_WORLD.allreduce(
+                        it_k, op=MPI.MAX)
+
+                    newton_iterations = max(it_k, newton_iterations)
 
                     # Adaptive timestepping a la Yibao Li et al. (2017)
-                    # TODO: Timestepping through free energy
                     u_max_loc = np.abs(self.u.sub(0).x.array -
                                        self.u0.sub(0).x.array).max()
 
                     u_err_max = self.comm.allreduce(u_max_loc, op=MPI.MAX)
 
-                    dt.value = min(max(tol / u_err_max, dt_min), dt_max, 1.1 * dt.value)
+                    dt_increase = 1.01 if it_voltage < max_it_voltage / 3 \
+                        else 1.001 if it_voltage < max_it_voltage / 2 \
+                        else 0.95 if it_voltage > max_it_voltage * 0.8 \
+                        else 1.0
+
+                    dt.value = min(max(tol / u_err_max, dt_min),
+                                   dt_max,
+                                   dt_increase * dt.value)
 
                     # callback(it, t, self.u)
 
-                except StopEvent as e:
+            # End of outer non-linear iteration loop with cond.
+            # (voltage_err > tol) and (it_voltage < max_it_voltage)
+            else:
 
-                    print(e)
-                    print(">>> Stop integration.")
-
-                    break
-
-                except RuntimeError as e:
-
-                    print(e)
-
+                # Catch the case when the iteration did not converge
+                if it_voltage >= max_it_voltage:
                     # reset and continue with smaller time step.
                     self.u.x.array[:] = self.u0.x.array[:]
 
-                    iterations = self.solver.max_iterations
-
+                    # Decrease timestep and repeat.
                     if dt.value > dt_min:
                         dt.value *= 0.5
-
                         print(f"Decrease timestep to dt={dt.value:1.3e}")
 
-                        continue
+                        continue  # This should reset the outer time loop
                     else:
-                        if self.output is not None:
-                            [o.save_snapshot(self.u, t) for o in self.output]
-
-                except ValueError as e:
-
-                    print(e)
-
-                    if self.output is not None:
-                        [o.save_snapshot(self.u, t, force=True)
-                         for o in self.output]
-
-                    break
+                        raise RuntimeError(f"Timestep too small {dt.value}.")
 
             # Find the minimum timestep among all processes.
-            # Note that we explicitly use COMM_WORLD since the mesh communicator
-            # only groups the processes belonging to one particle.
+            # Note that we explicitly use COMM_WORLD since the mesh
+            # communicator only groups the processes belonging to one particle.
             dt_global = MPI.COMM_WORLD.allreduce(dt.value, op=MPI.MIN)
 
             dt.value = dt_global
@@ -224,14 +242,13 @@ class Simulation(SimulationBase):
                 perc = t / self.T_final * 100
 
                 if MPI.COMM_WORLD.rank == 0:
-                    print(
+                    log(
                         f"{perc:>3.0f} % :",
                         f"t[{it:06}] = {t:1.6f}, "
                         f"dt = {dt.value:1.3e}, "
-                        f"its = {iterations}",
+                        f"its = {newton_iterations}",
                         f"its_voltage = {it_voltage}",
-                        f"voltage_inc = {voltage_inc}",
-                        flush=True
+                        f"V_OCP = {V_OCP}"
                     )
 
         else:
