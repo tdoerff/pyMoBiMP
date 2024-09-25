61f31ab538532ad12b71185dae74c72e9beffebb
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-09-25 17:11:01 +0200

    use regular grid for DFN

--- _under_development/CH_4_DFN_grid.py
+++ _under_development/CH_4_DFN_grid.py
@@ -547,57 +547,12 @@ class AnalyzeOCP(RuntimeAnalysisBase):
 
 def create_1p1_DFN_mesh(comm, n_rad=16, n_part=192):
 
-    # Nodes
-    # -----
-    radial_grid = np.linspace(0, 1, n_rad)
-    particle_grid = np.linspace(0, 1, n_part)
-
-    rr, pp = np.meshgrid(radial_grid, particle_grid)
-
-    coords_grid = np.stack((rr, pp)).transpose((-1, 1, 0)).copy()
-
-    if comm.rank == 0:
-        coords_grid_flat = coords_grid.reshape(-1, 2).copy()
-    else:
-        coords_grid_flat = np.empty((0, 2), dtype=np.float64)
-
-    # Elements
-    # --------
-    # All the radial connections
-    elements_radial = [
-        [[n_part * i + k, n_part * (i + 1) + k] for i in range(n_rad - 1)]
-        for k in range(n_part)
-    ]
-
-    elements_radial = np.array(elements_radial).reshape(-1, 2)
-
-    # Connections between particles
-    elements_bc = (n_rad - 1) * n_part + np.array([[k, k + 1] for k in range(n_part - 1)])
-    elements_bc = []  # With elements at the outer edge the integration fails.
-
-    if comm.rank == 0:
-        elements = np.array(list(elements_bc) + list(elements_radial))
-    else:
-        elements = np.empty((0, 2), dtype=np.int64)
-
-    # %% The DOLFINx grid
-    # -------------------
-
-    gdim = coords_grid_flat.shape[1]
-    shape = "interval"
-    degree = 1
-
-    domain = ufl.Mesh(
-        basix.ufl.element("Lagrange",
-                          shape,
-                          degree,
-                          shape=(gdim,)))
-
-    mesh = dfx.mesh.create_mesh(comm,
-                                elements[:, :gdim],
-                                coords_grid_flat,
-                                domain)
-
+    mesh = dfx.mesh.create_rectangle(
+        comm,
+        ((0.0, 0.0), (1.0, n_part)),
+        (n_rad, n_part),
+        cell_type=dfx.mesh.CellType.quadrilateral,
+    )
     return mesh
 
 
