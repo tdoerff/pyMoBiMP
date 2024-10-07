928ebbbf8895aa48fe145ed9140b92ed28ad5cec
Tom Doeffel <tom.doerffel@wias-berlin.de>
2024-10-07 13:28:46 +0200

    increase resolution and some more export to make viz run

--- tools/start_xvfb.sh
+++ tools/start_xvfb.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x
 export DISPLAY=:99.0
+export MESA_GL_VERSION_OVERRIDE=3.2
 export PYVISTA_OFF_SCREEN=true
+export PYVISTA_USE_IPYVTK=true
 which Xvfb
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+Xvfb :99 -screen 0 1920x1080x24 > /dev/null 2>&1 &
 sleep 3
 set +x
 exec "$@"
