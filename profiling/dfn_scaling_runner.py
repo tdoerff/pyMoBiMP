2c6c560631cab210e475c874f36b1e075f0c3c25
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-22 14:16:36 +0200

    update dfn_scaling_runner.py

--- profiling/dfn_scaling_runner.py
+++ profiling/dfn_scaling_runner.py
@@ -1,12 +1,11 @@
-import time
-
 import os
 
 from pyMoBiMP.dfn_battery_model import (
     AnalyzeOCP,
     ChargeDischargeExperiment,
     DefaultPhysicalSetup,
-    DFNSimulationBase)
+    DFNSimulationBase,
+    Timer)
 
 from pyMoBiMP.fenicsx_utils import FileOutput
 
@@ -17,39 +16,32 @@ class Simulation(DFNSimulationBase):
     RuntimeAnalysis = AnalyzeOCP
     Experiment = ChargeDischargeExperiment
 
+    def linear_solver_setup(self, solver):
 
-def mpi_time():
-
-    # Use the barriers to make sure every process starts and
-    # finishs at the same time and finishes
-    comm.barrier()
-    tic = time.perf_counter()
-
-    return tic
+        ksp = solver.ksp
+        ksp.setType("preonly")
+        ksp.getPC().setType("lu")
+        ksp.getPC().setFactorSolverType("mumps")
+        ksp.getPC().setFactorSetUpSolverType()
 
 
 if __name__ == "__main__":
 
     simulation = Simulation(
         n_particles=1024,
-        n_radius=16,
-        output_destination="_2_b_deleted"
+        n_radius=32,
+        output_destination="_2_b_deleted",
+        max_iterations=5,
     )
 
     comm = simulation.comm
 
-    tic = mpi_time()
-
-    simulation.run(dt_max=1e-4, n_out=0, t_final=0.05,)
-
-    toc = mpi_time()
-
-    # elapsed time
-    etime = toc - tic
+    with Timer(f"Running on {comm.size} procs") as timer:
+        simulation.run(dt_max=1e-2, n_out=0, t_final=0.05,)
 
     # Make sure the output file ends up in the same dir.
     output_file = os.path.dirname(__file__) + "/dfn_scaling.csv"
 
     if simulation.comm.rank == 0:
         with open(output_file, 'a') as file:
-            file.write(f"{comm.size} {etime}\n")
+            file.write(f"{comm.size} {timer.elapsed}\n")
