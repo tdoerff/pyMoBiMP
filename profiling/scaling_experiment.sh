b774cb66878fc4ac42dd65e23f5aa35f0589b2c4
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-22 11:37:01 +0200

    fix the scaling script

--- profiling/scaling_experiment.sh
+++ profiling/scaling_experiment.sh
@@ -1,11 +1,18 @@
 # Delete the old timing file.
 rm -f profiling/dfn_scaling.csv
 
+# Activate the conda environment
+conda activate fenicsx-env
 
+# Display the paths for mpirun and python
+which mpirun
+which python
+
+# Define the number of processors
 for n_procs in 1 2 4 8 16 32
 do
     for it in {1..4}
     do
-    mpirun -np $num_procs python profiling/dfn_scaling_runner.py
+        mpirun -np ${n_procs} python profiling/dfn_scaling_runner.py
     done
 done
