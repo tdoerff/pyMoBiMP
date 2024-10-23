52789458568adc04336197bec2a5e7d785671490
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-23 17:29:38 +0200

    remove 256 experiment

--- experiments/parameter_sweep_DFN/num_particles/0256/run.sh
+++
@@ -1,7 +0,0 @@
-set -x
-
-conda activate fenicsx-env
-
-touch running.lock
-python CH_4_DFN_grid.py 2>&1 > output.log
-rm running.lock
--- experiments/parameter_sweep_DFN/num_particles/0256/viz.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-# adjust accordingly!
-PYMOBIMP_BASE_DIR=${HOME}/Documents/Projects/single-particle-cahn-hilliard
-
-python ${PYMOBIMP_BASE_DIR}/tools/create_multi_particle_animation.py output -o anim.mp4 -e experiment --clipped
