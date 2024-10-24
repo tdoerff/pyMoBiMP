ea492794d9c6e335c49ac24233ce0ef1fd0c70ec
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-24 15:03:48 +0200

    add 10. diffusion experiment

---
+++ experiments/parameter_sweep_DFN/diffusion/10./experiment.py
@@ -0,0 +1,36 @@
+import os
+# Setting the number of OpenBLAS threads might be necessary if the OS complains
+# about busy resources.
+os.environ['OPENBLAS_NUM_THREADS'] = '1'
+
+from pyMoBiMP.battery_model import (  # noqa: 402
+    AnalyzeOCP,
+    ChargeDischargeExperiment,
+    DFNSimulationBase,
+    DefaultPhysicalSetup)
+
+from pyMoBiMP.fenicsx_utils import FileOutput  # noqa: 402
+
+
+class Simulation(DFNSimulationBase):
+    PhysicalSetup = DefaultPhysicalSetup
+    Output = FileOutput
+    RuntimeAnalysis = AnalyzeOCP
+    Experiment = ChargeDischargeExperiment
+
+    @staticmethod
+    def mobility(c):
+        return 1.0 * c * (1 - c)
+
+
+if __name__ == "__main__":
+
+    dir = os.path.dirname(__file__)
+
+    output_destination = dir + "/output"
+
+    simulation = Simulation(
+        n_particles=256,
+        output_destination=output_destination)
+
+    simulation.run(dt_max=1e-2, t_final=300.)
