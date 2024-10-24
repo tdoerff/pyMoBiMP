31c5a00491255334595c5f4af174587ef750dd34
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-24 10:40:50 +0200

    plot with r=1.0, only

--- experiments/parameter_sweep_DFN/num_particles/0032/viz.sh
+++ experiments/parameter_sweep_DFN/num_particles/0032/viz.sh
@@ -3,4 +3,4 @@
 # adjust accordingly!
 PYMOBIMP_BASE_DIR=${HOME}/Documents/Projects/single-particle-cahn-hilliard
 
-python ${PYMOBIMP_BASE_DIR}/tools/create_multi_particle_animation.py output -o anim.mp4 -e experiment --clipped -r 0.1
+python ${PYMOBIMP_BASE_DIR}/tools/create_multi_particle_animation.py output -o anim.mp4 -e experiment --clipped
