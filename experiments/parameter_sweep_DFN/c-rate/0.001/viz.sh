6addebc798c0f58292ceb6220a7b38d33df5cfe9
Tom Doerffel <tom.doerffel@wias-berlin.de>
2024-10-28 16:47:28 +0100

    make viz scripts path agnostic and add gamma=0.01 exp.

--- experiments/parameter_sweep_DFN/c-rate/0.001/viz.sh
+++ experiments/parameter_sweep_DFN/c-rate/0.001/viz.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 # adjust accordingly!
-PYMOBIMP_BASE_DIR=${HOME}/Documents/Projects/single-particle-cahn-hilliard
+PYMOBIMP_BASE_DIR=../../../../
 
 python ${PYMOBIMP_BASE_DIR}/tools/create_multi_particle_animation.py output -o anim.mp4 -e experiment --clipped
